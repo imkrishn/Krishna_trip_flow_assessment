65e93a667bf53139f118dcf8a38366b39f615fc9
imkrishn <963gurudev@gmail.com>
2026-03-06 09:54:28 +0530

    fix minor bug

--- frontend/components/ELDstatus.tsx
+++ frontend/components/ELDstatus.tsx
@@ -8,6 +8,8 @@ interface Props {
   trip: TripRouteResponse;
 }
 
+type TripStatus = "ONGOING" | "FINISHED" | "DRAFTED";
+
 export default function EldStatus({ trip }: Props) {
   const [status, setStatus] = useState<TripStatus>(trip.status);
   const [loading, setLoading] = useState(false);
